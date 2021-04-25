ccd2abfc0ca69a840a42d969c5e36821f1aba578
Deep Saha <deepsaha288@yahoo.com>
2021-04-25 20:12:57 +0530

    first game and first bet

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -1,6 +1,18 @@
+const WON = 1;
+const LOST = 0;
 var stake = 100;
 var bet = 1;
 
 console.log("Welcome to Gambling Simulator");
+
 console.log("Starting Stake for every day is: ",stake);
 console.log("Bet is: ",bet);
+
+var random = Math.floor(Math.random() * 2);
+
+if(random == WON){
+    stake += 1;
+}else{ 
+    stake -= 1;
+}
+console.log(stake);

95f7703e33e91627071a335744aa24a1d51231a7
Deep Saha <deepsaha288@yahoo.com>
2021-04-25 21:27:42 +0530

    asking for play again for player

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -17,75 +17,101 @@ console.log("Welcome to Gambling Simulator");
 console.log("Starting Stake for every day is: ",stake);
 console.log("Bet is: ",bet);
 
-for(let days = 1 ; days <= 20 ; days++){
-    stake = 100;
-    bets_lost = 0;
-    bets_won = 0;
-    while( stake > 50 && stake < 150){
-        var random = Math.floor(Math.random() * 2);
-        if(random == WON){
-            stake += bet;
-            bets_won += 1;
-            total_won += stake;
-        }else{ 
-            stake -= bet;
-            bets_lost += 1;
-            total_lost += stake;
+function startgame(){
+ 
+    for(let days = 1 ; days <= 20 ; days++){
+        stake = 100;
+        bets_lost = 0;
+        bets_won = 0;
+        while( stake > 50 && stake < 150){
+            var random = Math.floor(Math.random() * 2);
+            if(random == WON){
+                stake += bet;
+                bets_won += 1;
+                total_won += stake;
+            }else{ 
+                stake -= bet;
+                bets_lost += 1;
+                total_lost += stake;
+            }
+            month.set(days,stake);
+            max_won.set(days,bets_won);
+            max_lost.set(days,bets_lost);
         }
-        month.set(days,stake);
-        max_won.set(days,bets_won);
-        max_lost.set(days,bets_lost);
+    }
+
+    console.log("Total Amount Won: ",total_won);
+    console.log("Total Amount Lost: ",total_lost);
+        
+    if(total_won > total_lost){
+        console.log("You won  by: "+(total_won-total_lost));
+    }else{
+        console.log("You lost by: "+(total_lost-total_won));
+    }
+
+    getDayWonandLost();
+    getluckiestday();
+    getunlukiestday();
+}
+
+function getDayWonandLost(){
+    
+    for(let k of month.keys()){
+        if(month.get(k) == 150){
+            won.push(k);
+        }
+    }
+
+    for(let l of month.keys()){
+        if(month.get(l) == 50){
+            lost.push(l);
+        }
+    }
+
+    console.log("Days won are: "+won);
+    console.log("Days lost are: "+lost);
+}
+
+function getluckiestday(){
+    
+    var biggest = max_won.get(1);
+    for(let j of max_won.values()){
+        if(j > biggest){
+            biggest = j;
+        }
+    }
+    
+    for(let k of max_won.keys()){
+        if(max_won.get(k) == biggest){
+            console.log("The Luckiest Day is:",k);
+        }
     }
 }
 
-console.log(stake);
-console.log("Total Amount Won: ",total_won);
-console.log("Total Amount Lost: ",total_lost);
+function getunlukiestday(){
 
-for(let k of month.keys()){
-    if(month.get(k) == 150){
-        won.push(k);
+    var smallest = max_lost.get(1);
+    for(let f of max_lost.values()){
+        if(f < smallest){
+            smallest = f;
+        }
     }
+
+    for(let l of max_lost.keys()){
+        if(max_lost.get(l) == smallest){
+            console.log("The Unluckiest Day is:",l);
+        }
+    }
+    
 }
 
-for(let l of month.keys()){
-    if(month.get(l) == 50){
-        lost.push(l);
-    }
-}
+startgame();
 
-console.log("Days won are: "+won);
-console.log("Days lost are: "+lost);
+var string = require("readline-sync");
+var answer = string.question("Do you want to play again: ");
 
-if(total_won > total_lost){
-    console.log("You won  by: "+(total_won-total_lost));
+if(answer == "yes"){
+    startgame();
 }else{
-    console.log("You lost by: "+(total_lost-total_won));
-}
-
-var biggest = max_won.get(1);
-var smallest = max_lost.get(1);
-
-for(let j of max_won.values()){
-    if(j > biggest){
-        biggest = j;
-    }
-}
-
-for(let k of max_won.keys()){
-    if(max_won.get(k) == biggest){
-        console.log("The Luckiest Day is:",k);
-    }
-}
-
-for(let f of max_lost.values()){
-    if(f < smallest){
-        smallest = f;
-    }
-}
-
-for(let l of max_lost.keys()){
-    if(max_lost.get(l) == smallest){
-        console.log("The Unluckiest Day is:",l);
-    }
+    console.log("Thank you for playing")
 }

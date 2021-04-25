651a78c7a344fed0a1a4a3c7b540bcd8729392ce
Deep Saha <deepsaha288@yahoo.com>
2021-04-25 21:17:10 +0530

    display dayswon and lost how much

--- GamblingSimulator.js
+++ GamblingSimulator.js
@@ -4,6 +4,9 @@ var stake = 100;
 var bet = 1;
 var total_won = 0;
 var total_lost = 0;
+var month = new Map();
+var won = [];
+var lost = [];
 
 console.log("Welcome to Gambling Simulator");
 
@@ -11,9 +14,9 @@ console.log("Starting Stake for every day is: ",stake);
 console.log("Bet is: ",bet);
 
 for(let days = 1 ; days <= 20 ; days++){
+    stake = 100;
     while( stake > 50 && stake < 150){
         var random = Math.floor(Math.random() * 2);
-
         if(random == WON){
             stake += bet;
             total_won += stake;
@@ -21,8 +24,30 @@ for(let days = 1 ; days <= 20 ; days++){
             stake -= bet;
             total_lost += stake;
         }
+        month.set(days,stake);
     }
 }
 console.log(stake);
 console.log("Total Amount Won: ",total_won);
 console.log("Total Amount Lost: ",total_lost);
+
+for(let k of month.keys()){
+    if(month.get(k) == 150){
+        won.push(k);
+    }
+}
+
+for(let l of month.keys()){
+    if(month.get(l) == 50){
+        lost.push(l);
+    }
+}
+
+console.log("Days won are: "+won);
+console.log("Days lost are: "+lost);
+
+if(total_won > total_lost){
+    console.log("You won  by: "+(total_won-total_lost));
+}else{
+    console.log("You lost by: "+(total_lost-total_won));
+}
